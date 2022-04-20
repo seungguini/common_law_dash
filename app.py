a00e34f2f2073373d29ec486fb37937aca1fed09
seungguini <sl6369@nyu.edu>
2022-04-20 16:19:20 -0400

    Continued effort in making subplots work

--- app.py
+++ app.py
@@ -9,33 +9,36 @@ from utils import calculate_differences
 Create a grouped bar chart of annotation differences per each round
 """
 
+NUMBER_OF_GROUPS = 4
 
-
-def create_chart(df, group_no, category):
+def create_chart(df, category):
     fig = make_subplots(
         rows=4,  # Group per row
-        cols=2,  # Differences, distribution
+        cols=1,  # Differences, distribution
         subplot_titles=(
-            "Group 1: Differences in Annotations", "Group 1: Distribution of Annotations (Total)",
-            "Group 2: Differences in Annotations", "Group 1: Distribution of Annotations (Total)",
-            "Group 3: Differences in Annotations", "Group 1: Distribution of Annotations (Total)",
-            "Group 4: Differences in Annotations", "Group 1: Distribution of Annotations (Total)",
+            "Group 1: Differences in Annotations",# "Group 1: Distribution of Annotations (Total)",
+            "Group 2: Differences in Annotations",# "Group 1: Distribution of Annotations (Total)",
+            "Group 3: Differences in Annotations",# "Group 1: Distribution of Annotations (Total)",
+            "Group 4: Differences in Annotations",# "Group 1: Distribution of Annotations (Total)",
         )
     )
-    for group_no in
-    fig = go.Figure(
-        data=[
-            go.Bar(
-                name="Difference: " + str(difference),  # Dropdown category
-                x=df[df['group'] == group_no][df['difference'] == difference][df['category'] == category]['round'],
-                y=df[df['group'] == group_no][df['difference'] == difference][df['category'] == category]['value'],
-                offsetgroup=idx
-            ) for idx, difference in enumerate(range(0, 5))
-        ]
-    )
+    for idx, group_no in enumerate(range(NUMBER_OF_GROUPS)):
 
-    fig.update_xaxes(title_text='Round')
-    fig.update_yaxes(title_text='Count')
+        graphs = [
+                go.Bar(
+                    name="Difference: " + str(difference),  # Dropdown category
+                    x=df[df['group'] == group_no][df['difference'] == difference][df['category'] == category]['round'],
+                    y=df[df['group'] == group_no][df['difference'] == difference][df['category'] == category]['value'],
+                    offsetgroup=idx
+                ) for idx, difference in enumerate(range(0, 5))
+            ]
+
+        # for graph in graphs:
+        #     graph.
+        #     # graph.update_xaxes(title_text='Round')
+        #     # graph.update_yaxes(title_text='Count')
+
+        fig.append_trace(graphs[0], row=idx+1, col=1)
 
     return fig
 
@@ -45,12 +48,12 @@ def create_chart(df, group_no, category):
 master_df = calculate_differences()
 ANNOTATION_CATEGORIES = ['Appropriateness', 'Information content of outputs', 'Humanlikeness']
 
-fig = create_chart(master_df, 1, 'Appropriateness')
+fig = create_chart(master_df, 'Appropriateness')
 
 # ===== UPDATE AXES =====
 
-fig.update_xaxes(title_text='Round')
-fig.update_yaxes(title_text='Count')
+# fig.update_xaxes(title_text='Round')
+# fig.update_yaxes(title_text='Count')
 
 print(master_df.size)
 app = Dash(__name__)
@@ -95,6 +98,7 @@ app.layout = html.Div(children=[
 
 
 # ===== CALLBACKS FOR DROPDOWNS =====
+"""
 
 # Store dropdown values to local session storage
 @app.callback(
@@ -116,6 +120,6 @@ def update_dropdown_values(group_no, category):
 def update_category_chart(filter_json):
     return create_chart(master_df, filter_json['group_no'], filter_json['category'])
 
-
+"""
 if __name__ == '__main__':
     app.run_server(debug=True)
